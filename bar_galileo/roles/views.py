bc69544b40ae0024bf39e8cec66a0ef833fd4fc0
Jorge Alfredo Arismendy Zambrano <jorgealfredo.arismendy@example.com>
2025-07-30 10:33:10 -0500

    cambios en el control para los permisos, y arreglo de bugs en el modulo de roles

--- bar_galileo/roles/views.py
+++ bar_galileo/roles/views.py
@@ -1,43 +1,56 @@
+from django.views import View
+from django.views.generic import ListView, CreateView
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Role, RolePermission, Module, Action
-from .forms import RoleForm, RolePermissionFormSet
-from django.urls import reverse
+from django.urls import reverse_lazy, reverse
 from django.contrib import messages
 
-def rol_list(request):
-    roles = Role.objects.all()
-    return render(request, 'roles/rol_list.html', {'roles': roles})
+from .models import Role, RolePermission, Module, Action
+from .forms import RoleForm
+from django import template
+register = template.Library()
 
-def rol_create(request):
-    if request.method == 'POST':
-        form = RoleForm(request.POST)
-        if form.is_valid():
-            role = form.save()
-            return redirect('roles:rol_permisos', role.id)
-    else:
-        form = RoleForm()
-    return render(request, 'roles/rol_form.html', {'form': form})
+class RolListView(ListView):
+    model = Role
+    template_name = 'roles/rol_list.html'
+    context_object_name = 'roles'
 
-def rol_permisos(request, role_id):
-    role = get_object_or_404(Role, id=role_id)
-    modulos = Module.objects.all()
-    acciones = Action.objects.all()
-    permisos = RolePermission.objects.filter(rol=role)
-    permisos_dict = {f'{p.modulo_id}_{p.accion_id}': p for p in permisos}
+class RolCreateView(CreateView):
+    model = Role
+    form_class = RoleForm
+    template_name = 'roles/rol_form.html'
 
-    if request.method == 'POST':
+    def form_valid(self, form):
+        role = form.save()
+        return redirect('roles:rol_permisos', role.id)
+
+
+class RolPermisosView(View):
+    def get(self, request, role_id):
+        role = get_object_or_404(Role, id=role_id)
+        modulos = Module.objects.all()
+        acciones = Action.objects.all()
+        permisos = RolePermission.objects.filter(rol=role)
+        
+        return render(request, 'roles/rol_permisos.html', {
+            'role': role,
+            'modulos': modulos,
+            'acciones': acciones,
+            'permisos': permisos,  # Pasamos los permisos directamente
+        })
+
+    def post(self, request, role_id):
+        role = get_object_or_404(Role, id=role_id)
+        modulos = Module.objects.all()
+        acciones = Action.objects.all()
+
         # Limpiar permisos existentes
         RolePermission.objects.filter(rol=role).delete()
+        
+        # Crear permisos nuevos marcados
         for modulo in modulos:
             for accion in acciones:
                 if request.POST.get(f"perm_{modulo.id}_{accion.id}"):
                     RolePermission.objects.create(rol=role, modulo=modulo, accion=accion)
-        messages.success(request, 'Permisos actualizados')
-        return redirect('roles:rol_list')
 
-    return render(request, 'roles/rol_permisos.html', {
-        'role': role,
-        'modulos': modulos,
-        'acciones': acciones,
-        'permisos_dict': permisos_dict,
-    })
+        messages.success(request, 'Permisos actualizados correctamente.')
+        return redirect('roles:rol_list')
