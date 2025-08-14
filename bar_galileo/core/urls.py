c05dd27919fe369a5d39bd897472a27b7c4d40fd
Jorge Alfredo Arismendy Zambrano <jorgealfredo.arismendy@example.com>
2025-08-14 11:00:21 -0500

    Revert "Christian"

--- bar_galileo/core/urls.py
+++ bar_galileo/core/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import indexView, ProductosAjaxView
+from .views import indexView
 
 urlpatterns = [
     path('', indexView.as_view(), name='index'),
-    path('productos/ajax/', ProductosAjaxView.as_view(), name='productos_ajax'),
 ]
