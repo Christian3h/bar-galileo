b2c2030869d84959ddb6e16572213a183355b109
Jorge Alfredo Arismendy Zambrano <jorgealfredo.arismendy@example.com>
2025-08-23 21:56:05 -0500

    merge con la rama Sebastian que incluse panel de usuario

--- bar_galileo/roles/views.py
+++ bar_galileo/roles/views.py
@@ -1,15 +1,18 @@
 from django.views import View
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy, reverse
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils.decorators import method_decorator
+from roles.decorators import permission_required
 
 from .models import Role, RolePermission, Module, Action
 from .forms import RoleForm
 from django import template
 register = template.Library()
 
+@method_decorator(permission_required('roles', 'ver'), name='dispatch')
 class RolListView(ListView):
     model = Role
     template_name = 'roles/rol_list.html'
@@ -17,6 +20,7 @@ class RolListView(ListView):
 
 from notifications.utils import notificar_usuario
 
+@method_decorator(permission_required('roles', 'crear'), name='dispatch')
 class RolCreateView(LoginRequiredMixin, CreateView):
     model = Role
     form_class = RoleForm
@@ -24,11 +28,36 @@ class RolCreateView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         role = form.save()
-        mensaje = f"Se ha creado el nuevo rol: '{role.name}'."
+        mensaje = f"Se ha creado el nuevo rol: '{role.nombre}'."
         notificar_usuario(self.request.user, mensaje)
         return redirect('roles:rol_permisos', role.id)
 
+@method_decorator(permission_required('roles', 'editar'), name='dispatch')
+class RolUpdateView(LoginRequiredMixin, UpdateView):
+    model = Role
+    form_class = RoleForm
+    template_name = 'roles/rol_form.html'
+    success_url = reverse_lazy('roles:rol_list')
 
+    def form_valid(self, form):
+        role = form.save()
+        mensaje = f"El rol '{role.nombre}' ha sido actualizado."
+        notificar_usuario(self.request.user, mensaje)
+        return redirect(self.success_url)
+
+@method_decorator(permission_required('roles', 'eliminar'), name='dispatch')
+class RolDeleteView(LoginRequiredMixin, DeleteView):
+    model = Role
+    template_name = 'roles/rol_confirm_delete.html'
+    success_url = reverse_lazy('roles:rol_list')
+
+    def form_valid(self, form):
+        role = self.get_object()
+        mensaje = f"El rol '{role.nombre}' ha sido eliminado."
+        notificar_usuario(self.request.user, mensaje)
+        return super().form_valid(form)
+
+@method_decorator(permission_required('roles', 'editar'), name='dispatch')
 class RolPermisosView(LoginRequiredMixin, View):
     def get(self, request, role_id):
         role = get_object_or_404(Role, id=role_id)
