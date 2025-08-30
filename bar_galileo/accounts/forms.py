d503ae6f1fbcb8c6d220ffbbf39047dbbe6e2685
Jorge Alfredo Arismendy Zambrano <jorgealfredo.arismendy@example.com>
2025-08-30 15:45:07 -0500

    audio funcional para el captcha

--- bar_galileo/accounts/forms.py
+++ bar_galileo/accounts/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 from allauth.account.forms import LoginForm
-from captcha.fields import CaptchaField, CaptchaTextInput
+from captcha.fields import CaptchaField
 
 class CustomLoginForm(LoginForm):
-    captcha = CaptchaField()
+    captcha = CaptchaField(label='Captcha')
 
     def __init__(self, *args, **kwargs):
         super(CustomLoginForm, self).__init__(*args, **kwargs)
