115297136d1a5b986dac1cd416e8b54be2e4a5c3
christian <christianmartinez3h@gmail.com>
2025-09-10 08:24:01 -0500

    cambios varios xd

--- bar_galileo/accounts/forms.py
+++ bar_galileo/accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from allauth.account.forms import LoginForm
+from allauth.account.forms import LoginForm, AddEmailForm
+from allauth.account.models import EmailAddress
 from captcha.fields import CaptchaField
 
 class CustomLoginForm(LoginForm):
@@ -9,3 +10,15 @@ class CustomLoginForm(LoginForm):
         super(CustomLoginForm, self).__init__(*args, **kwargs)
         self.fields['login'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Usuario'})
         self.fields['password'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Contraseña'})
+
+class CustomAddEmailForm(AddEmailForm):
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+        super(CustomAddEmailForm, self).__init__(*args, **kwargs)
+
+    def clean_email(self):
+        email = super().clean_email()
+        if self.request and self.request.user.is_authenticated:
+            if self.request.user.emailaddress_set.filter(email__iexact=email).exists():
+                raise forms.ValidationError("Esta dirección de correo ya está asociada a tu cuenta.")
+        return email
