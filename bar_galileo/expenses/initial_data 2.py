5eca38b7e7c5545d713f07f0b64578aed8cbe0ff
christian <christianmartinez3h@gmail.com>
2025-10-25 22:36:38 -0500

    chore: Eliminar archivos duplicados del merge
    - Remover todos los archivos con sufijo ' 2' creados durante el merge
    - Limpieza de archivos redundantes en static/, templates/ y módulos Python

--- bar_galileo/expenses/initial_data 2.py
+++
@@ -1,7 +0,0 @@
-
-from .models import ExpenseCategory
-
-def create_expense_categories():
-    categories = ['Gastos Operativos', 'Gastos Extraordinarios']
-    for category in categories:
-        ExpenseCategory.objects.get_or_create(name=category)
